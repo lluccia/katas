c46a6e342e3ee04d3fa72408dede5bc57a8143d6
Leandro Conca <lluccia@gmail.com>
2016-03-15 00:22:51 -0300

    refactor add logic and included ignore numbers > than 1000

--- string-calculator/src/test/java/StringCalculatorTest.java
+++ string-calculator/src/test/java/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -52,31 +53,65 @@ public class StringCalculatorTest {
         }
     }
 
+    @Test
+    public void shouldIgnoreNumbersGreaterThan1000() {
+        assertEquals(2, add("1001,2"));
+    }
+
+
     private int add(String calcString) {
-        if ("".equals(calcString)) {
-            return 0;
-        } else {
-            String delimiter = getDelimiter(calcString);
-            String[] operands = getOperands(calcString).split(delimiter);
-            validateOperands(operands);
-            int sum = 0;
+        int sum = 0;
 
-            for (String operand: operands)
-                sum += Integer.valueOf(operand);
+        for (Integer operand: getValidOperands(calcString))
+            sum += operand;
 
-            return sum;
+        return sum;
+    }
+
+    private List<Integer> getValidOperands(String calcString) {
+        if ("".equals(calcString))
+            return new ArrayList<>();
+
+        String delimiter = getDelimiter(calcString);
+
+        String[] operandsSplit = getOperands(calcString).split(delimiter);
+
+        List<Integer> integers = convertToInt(operandsSplit);
+
+        validateOperands(integers);
+
+        removeNumbersGreaterThan1000(integers);
+
+        return integers;
+    }
+
+    private List<Integer> convertToInt(String[] operandsSplit) {
+        List<Integer> integers = new ArrayList<>();
+        for (String operand: operandsSplit)
+            integers.add(Integer.valueOf(operand));
+
+        return integers;
+    }
+
+    private void removeNumbersGreaterThan1000(List<Integer> operands) {
+        Iterator<Integer> iterator = operands.iterator();
+
+        while(iterator.hasNext()) {
+            Integer next = iterator.next();
+            if (next > 1000)
+                iterator.remove();
         }
     }
 
-    private void validateOperands(String[] operands) {
-        List<String> invalidOperands = new ArrayList<>();
-        for (String operand: operands)
+    private void validateOperands(List<Integer> operands) {
+        List<Integer> invalidOperands = new ArrayList<>();
+        for (Integer operand: operands)
             if (Integer.valueOf(operand) < 0)
                 invalidOperands.add(operand);
 
         if (invalidOperands.size() > 0) {
             String invalidOperandsMsg = "Negatives are not allowed: ";
-            for (String invalidOperand: invalidOperands) {
+            for (Integer invalidOperand: invalidOperands) {
                 invalidOperandsMsg += invalidOperand + ",";
             }
             invalidOperandsMsg = invalidOperandsMsg.substring(0, invalidOperandsMsg.length() - 1);
